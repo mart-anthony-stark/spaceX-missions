860be8bca20cd7714a8def544e1cd011cc72255e
mart-anthony-stark <martsalazar0@gmail.com>
2021-07-24 21:29:23 -0700

    1 file modified

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -3,7 +3,18 @@ import React from 'react';
 export default function Footer() {
   return (
     <div className="footer">
-      <p>Created By: Mart Anthony A. Salazar</p>
+      <div className="contact">
+        <p>Created By: Mart Anthony A. Salazar</p>
+        <a href="https://github.com/mart-anthony-stark" target="_blank">
+          <i class="fa fa-github" aria-hidden="true" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/mart-anthony-salazar-420ba81b6/"
+          target="_blank"
+        >
+          <i class="fa fa-linkedin-square" aria-hidden="true" />
+        </a>
+      </div>
       <p>&copy;All rights reserved.</p>
       <p>Api used: https://api.spacexdata.com/v3/missions</p>
     </div>
