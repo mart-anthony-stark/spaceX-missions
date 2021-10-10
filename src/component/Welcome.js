7b3012ff5f7dd754fff3712db2e30f62587a247b
mart-anthony-stark <martsalazar0@gmail.com>
2021-10-10 19:41:31 +0800

    1 file modified

--- src/component/Welcome.js
+++ src/component/Welcome.js
@@ -15,19 +15,19 @@ export default function Welcome({ info, tab, links }) {
 
       <div className="socials">
         <a href={links.website} target="_blank">
-          <i class="fa fa-globe" aria-hidden="true" />
+          <i className="fa fa-globe" aria-hidden="true" />
           Website
         </a>
         <a href={links.flickr} target="_blank">
-          <i class="fa fa-flickr" aria-hidden="true" />
+          <i className="fa fa-flickr" aria-hidden="true" />
           Flickr
         </a>
         <a href={links.twitter} target="_blank">
-          <i class="fa fa-twitter" aria-hidden="true" />
+          <i className="fa fa-twitter" aria-hidden="true" />
           Twitter
         </a>
         <a href={links.elon_twitter} target="_blank">
-          <i class="fa fa-twitter-square" aria-hidden="true" />
+          <i className="fa fa-twitter-square" aria-hidden="true" />
           Elon's Twitter
         </a>
       </div>
